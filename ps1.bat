dcf403e47a74311714a246a062271e2177117f60
th5528e <tagan672@gmail.com>
2025-06-05 20:08:49 +0100

    creation of current version value branch

--- ps1.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:start:
-setlocal EnableDelayedExpansion
-title %~n0
-set "batdir=%~dp0"
-pushd "%batdir%"
-
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%batdir%settimestamps.ps1" -Folder "%batdir%compressed"
